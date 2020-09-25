56dc61e51364f84788e8b0dbcf68c936c4e35f37
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-09-25 08:24:08 -0500

    Added a type property in ChatChannel

--- lib/models/chat_channel.dart
+++ lib/models/chat_channel.dart
@@ -1,22 +1,36 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+enum CHAT_CHANNEL_TYPE { SIGN_IN, ANONYMOUS, SIGN_IN_OR_ANONYMOUS }
+
 class ChatChannel {
   String id;
   String name;
   String description;
+  CHAT_CHANNEL_TYPE type;
 
-  ChatChannel({
-    this.id,
-    this.name,
-    this.description,
-  });
+  ChatChannel({this.id, this.name, this.description, this.type});
 
   factory ChatChannel.fromDocumentSnapshot(
       DocumentSnapshot chatChannelFirebase) {
+    CHAT_CHANNEL_TYPE type;
+    String typeString = chatChannelFirebase['type'];
+    switch (typeString) {
+      case 'sign_in':
+        type = CHAT_CHANNEL_TYPE.SIGN_IN;
+        break;
+      case 'anonymous':
+        type = CHAT_CHANNEL_TYPE.ANONYMOUS;
+        break;
+      case 'sign_in_or_anonymous':
+      default:
+        type = CHAT_CHANNEL_TYPE.SIGN_IN_OR_ANONYMOUS;
+    }
+
     return ChatChannel(
       id: chatChannelFirebase.documentID,
       name: chatChannelFirebase.data['name'],
       description: chatChannelFirebase.data['description'],
+      type: type,
     );
   }
 }
