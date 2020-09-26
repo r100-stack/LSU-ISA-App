d152847579740a07b53a6f098afe5eda3f559cb5
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-09-25 22:13:58 -0500

    Some complex implementation for the time in the ChatMessageCard. Deleting it now and implementing a simpler version

--- lib/screens/chat_screen/widgets/chat_message_card.dart
+++ lib/screens/chat_screen/widgets/chat_message_card.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -43,6 +45,7 @@ class ChatMessageCard extends StatelessWidget {
               const SizedBox(
                 height: kDefaultMargin / 2,
               ),
+              _getRespectiveTimestamp(chatMessage.timestamp),
               Text(
                 _getDisplayTime(chatMessage.timestamp),
                 // '${dateFormat.format(chatMessage.timestamp)}',
@@ -55,7 +58,40 @@ class ChatMessageCard extends StatelessWidget {
     );
   }
 
-  String _getDisplayTime(DateTime timestamp) {
+  Widget _getRespectiveTimestamp(DateTime timestamp) {
+    final DateFormat timeFormat = DateFormat('hh:mm aa');
+    final DateFormat dateFormat = DateFormat('MMM dd, yyyy');
+    String time = '${timeFormat.format(chatMessage.timestamp)}';
+
+    DateTime now = DateTime.now();
+
+    String text;
+    // Between 1 minute and 1 hour
+    if (timestamp.day == now.day &&
+        now.difference(timestamp) >= Duration(minutes: 1) &&
+        now.difference(timestamp) < Duration(hours: 1)) {
+      text = timeago.format(timestamp);
+    }
+    // Difference in 1 day
+    if (timestamp.day != now.day &&
+        now.difference(timestamp) <= Duration(days: 1)) {
+      text = 'Yesterday $time';
+    }
+    // Same day
+    if (timestamp.day == now.day &&
+        now.difference(timestamp) <= Duration(days: 1)) {
+      text = 'Today $time';
+    }
+    text = '${dateFormat.format(timestamp)} $time';
+  }
+
+  /**
+   * Returns a list with first parameter as the text, and the second as 
+   * a bool for isTimeago
+   */
+  List<dynamic> _getDisplayText(DateTime timestamp) {
+    Timer.periodic(Duration(minutes: 1), (Timer t) => )
+
     final DateFormat timeFormat = DateFormat('hh:mm aa');
     final DateFormat dateFormat = DateFormat('MMM dd, yyyy');
     String time = '${timeFormat.format(chatMessage.timestamp)}';
@@ -65,18 +101,18 @@ class ChatMessageCard extends StatelessWidget {
     if (timestamp.day == now.day &&
         now.difference(timestamp) >= Duration(minutes: 1) &&
         now.difference(timestamp) < Duration(hours: 1)) {
-      return timeago.format(timestamp);
+      return [timeago.format(timestamp), true];
     }
     // Difference in 1 day
     if (timestamp.day != now.day &&
         now.difference(timestamp) <= Duration(days: 1)) {
-      return 'Yesterday $time';
+      return ['Yesterday $time', false];
     }
     // Same day
     if (timestamp.day == now.day &&
         now.difference(timestamp) <= Duration(days: 1)) {
-      return 'Today $time';
+      return ['Today $time', false];
     }
-    return '${dateFormat.format(timestamp)} $time';
+    return ['${dateFormat.format(timestamp)} $time'];
   }
 }
